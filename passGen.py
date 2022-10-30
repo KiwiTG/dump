b1841de9002c59ad14bcd43322e7c0e87ae57b3d
H4Z3 <93849885+KiwiTG@users.noreply.github.com>
2022-10-30 15:39:33 -0400

    Create passGen.py

---
+++ passGen.py
@@ -0,0 +1,19 @@
+import string, random, argparse
+
+parser = argparse.ArgumentParser(description='Strong Password Generator')
+parser.add_argument('-l', '--length', metavar='NUM', required=True, type=int, default=50, help='Password Length')
+args = parser.parse_args()
+
+howmany = args.length
+
+characters = list(string.ascii_letters + string.digits + '!@#$%^&*()')
+
+random.shuffle(characters)
+
+password = []
+for i in range(howmany):
+	password.append(random.choice(characters))
+
+random.shuffle(password)
+
+print("".join(password))
